1d57fbd471fc8e663ed534f0f08f3e290e0a30ca
syed <syedyasirkamal@gmail.com>
2022-12-08 16:23:53 -0500

    ci-cd

--- application.py
+++ application.py
@@ -1,7 +1,9 @@
 #          Import some packages               #
 ###############################################
 from flask import Flask, render_template, request
-
+from flask_mysqldb import MySQL
+from flask_mail import Mail, Message
+from models import signupForm
 
 
 ###############################################
